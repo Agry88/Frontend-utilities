acca028d7f281737ba9d45ae9863f7876228f624
agry88 <rgok307085@gmail.com>
2022-09-16 10:36:20 +0800

    Feat:Complete Resize

--- mainnav/script.js
+++ mainnav/script.js
@@ -2,9 +2,17 @@ document.addEventListener("DOMContentLoaded",()=>{
     const btn_shopcar = document.querySelector("body > section > header.mainnav-wrapper > div.mainnav-right-block > div > div.shopcar-section > button")
     const popout_shopcar = document.querySelector("body > section > header.mainnav-wrapper > div.mainnav-right-block > div > div.shopcar-section > div")
     btn_shopcar.addEventListener("click",()=>{
-        popout_shopcar.setAttribute("aria-display","true")
+        const display = popout_shopcar.getAttribute("aria-display")
+        const returndisplay = display==="true" ? "false" : "true"
+        popout_shopcar.setAttribute("aria-display",returndisplay)
     })
-    btn_shopcar.addEventListener("focusout",()=>{
-        popout_shopcar.setAttribute("aria-display","false")
+
+    const btn_menu = document.querySelector("#btn-mainnav-menu")
+    const right_block = document.querySelector("body > section > header.mainnav-wrapper > div.mainnav-right-block > div")
+    btn_menu.addEventListener("click",()=>{
+        const display = right_block.getAttribute("aria-expanded")
+        const returndisplay = display==="true" ? "false" : "true"
+        right_block.setAttribute("aria-expanded",returndisplay)
     })
+
 })
